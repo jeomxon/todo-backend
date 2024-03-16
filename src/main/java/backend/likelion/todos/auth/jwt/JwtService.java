70f551521e5a57ef39982dfb7230ac58507388fb
jeomxon <jeomxon@gmail.com>
2024-03-17 00:09:25 +0900

    level5 구현

--- src/main/java/backend/likelion/todos/auth/jwt/JwtService.java
+++ src/main/java/backend/likelion/todos/auth/jwt/JwtService.java
@@ -5,8 +5,10 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 import backend.likelion.todos.common.UnAuthorizedException;
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import java.util.Date;
 import org.springframework.stereotype.Service;
 
@@ -24,15 +26,23 @@ public class JwtService {
 
     // 회원 ID를 기반으로 JWT 토큰을 생성합니다.
     public String createToken(Long memberId) {
-        // TODO [5단계] 현재 시간과 설정된 만료 시간을 사용하여 만료 날짜를 설정하세요.
-        // TODO [5단계] memberId를 클레임으로 추가하세요.
-        // TODO [5단계] 설정된 알고리즘으로 토큰을 서명하고 반환하세요.
-        return null;
+        final Date now = new Date();
+        return JWT.create()
+                .withIssuedAt(now)
+                .withExpiresAt(new Date(now.getTime() + accessTokenExpirationDayToMills))
+                .withClaim("memberId", memberId)
+                .sign(algorithm);
     }
 
     // 토큰에서 회원 ID를 추출합니다.
     public Long extractMemberId(String token) {
-        // TODO [5단계] 알고리즘을 사용해 토큰의 유효성을 검증하고, "memberId" 클레임에서 회원 ID를 추출하세요. 유효하지 않은 경우 "유효하지 않은 토큰입니다." 메시지와 함께 UnAuthorizedException을 발생시키세요.
-        return null;
+        try {
+            JWTVerifier jwtVerifier = JWT.require(algorithm).build();
+            DecodedJWT decodedJwt = jwtVerifier.verify(token);
+            return decodedJwt.getClaim("memberId")
+                    .asLong();
+        } catch (JWTVerificationException e) {
+            throw new UnAuthorizedException("유효하지 않은 토큰입니다.");
+        }
     }
 }
