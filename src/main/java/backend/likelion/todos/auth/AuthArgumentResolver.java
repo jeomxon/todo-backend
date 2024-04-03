7d9bcce6ef4f205ed0f6a387c7945054193fcb75
jeomxon <jeomxon@gmail.com>
2024-04-03 22:13:02 +0900

    level6 구현

--- src/main/java/backend/likelion/todos/auth/AuthArgumentResolver.java
+++ src/main/java/backend/likelion/todos/auth/AuthArgumentResolver.java
@@ -4,6 +4,7 @@ import backend.likelion.todos.auth.jwt.JwtService;
 import backend.likelion.todos.common.UnAuthorizedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -19,8 +20,9 @@ public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        // TODO [6단계] parameter가 @Auth 어노테이션을 갖고 있고, 파라미터 타입이 Long.class인 경우 true를 반환하는 조건을 구현하세요.
-        return false;
+        boolean isCorrectParameterType = parameter.getParameterType().equals(Long.class);
+        boolean hasAnnotation = parameter.hasParameterAnnotation(Auth.class);
+        return isCorrectParameterType && hasAnnotation;
     }
 
     @Override
@@ -30,13 +32,17 @@ public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
             NativeWebRequest webRequest,
             WebDataBinderFactory binderFactory
     ) {
-        // TODO [6단계] webRequest로부터 accessToken을 추출하고, jwtService를 사용하여 memberId를 추출하여 반환하는 로직을 구현하세요.
-        return null;
+        String accessToken = extractAccessToken(webRequest);
+        Long memberId = jwtService.extractMemberId(accessToken);
+        return memberId;
     }
 
     private static String extractAccessToken(NativeWebRequest request) {
-        // TODO [6단계] request 헤더에서 "Authorization" 헤더 값을 추출하여 "Bearer "로 시작하는 accessToken을 반환하세요. 유효하지 않을 경우 "로그인 후 접근할 수 있습니다." 메시지와 함께 UnAuthorizedException을 발생시키는 로직을 구현하세요.
-        return null;
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (StringUtils.hasText(token) && token.startsWith("BEARER ")) {
+            return token.split(" ")[1];
+        }
+        throw new UnAuthorizedException("로그인 후 접근할 수 있습니다.");
     }
 
 }
